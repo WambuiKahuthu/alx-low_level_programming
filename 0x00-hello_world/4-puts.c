ebcd5f016b0e5203013b3bef0ca54db0a2568d2f
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-08 06:03:57 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
+/*
+ *main - entry point to the program
+ *return - indicate how program exited
+ */
