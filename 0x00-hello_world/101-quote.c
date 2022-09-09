ec970197c23539d8b984de8de21143e5704046cf
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-08 19:21:44 -0700

    fwrite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - entry point
+/**
+ * main - Entry point
  * Return: shows how program exited
  */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
 	fflush(stdout);
 	return (2);
