c218d4a1027842a613ce19ea9dea22f1e8a4c70e
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-10 00:50:37 +0300

    Print base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: successful exit is 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+  
+  putchar('\n')
+	return (0);
+}
