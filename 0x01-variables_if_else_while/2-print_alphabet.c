a1d51a41fec505acd2a21bf8917cefd1a38a0d3d
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-09 07:45:35 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: indicates how the program exited
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	/* print alphabet from a to z using for loop*/
+	
+	putchar('\n');
+	return (0);
+}
