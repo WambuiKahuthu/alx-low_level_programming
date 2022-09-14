a4ff87c25bab76071e9a7c4d21ac623e875a4648
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-15 01:43:51 +0300

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+
+/**
+ * main - prints sum of even fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long n1 = 0, n2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = n1 + n2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
+
+		n1 = n2;
+		n2 = sum;
+	}
+	printf("%.0f\n", total);
+
+	return (0);
+}
