84ed3b3c36e032d57106df95ac342fc858577ed5
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-14 12:29:31 -0700

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 {
 	int x, ch;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		for (ch = 97; ch <= 122; ch++)
 			_putchar(ch);
-	
+
 	_putchar('\n');
 	}
 }
