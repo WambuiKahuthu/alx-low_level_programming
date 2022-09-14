806b791b7f1a8de3f13a95a08f523519aa8fc0a8
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-15 00:45:50 +0300

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_table - prints the 9 times table
  */
- 
+
 void times_table(void)
 {
 	int n, num, ans;
@@ -14,10 +14,10 @@ void times_table(void)
 		for (num = 1; num <= 9; num++)
 		{
 			ans = n * num;
-      
+
 			_putchar(44);
 			_putchar(32);
-      
+
 			if (ans <= 9)
 			{
 				_putchar(32);
