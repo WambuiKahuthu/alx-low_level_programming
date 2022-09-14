cba993a6432bb78126988e4ec61930bfa9fed8e5
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-14 10:50:17 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[]= " _putchar";
+	int count, size;
+
+	size = sizeof(str) / sizeof(char);
+
+	for(count = 0; count < size; count++)
+		_putchar(str[count]);
+
+	_putchar('\n');
+	return (0);
+}
