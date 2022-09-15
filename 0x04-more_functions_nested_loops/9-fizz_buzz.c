e3e5ea27995fd031969012de1e5b7b3d72fb2b6c
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-15 16:47:59 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print 1 to 100
+ * replace multiples of 3 with Fizz
+ *         multiples of 5 with Buzz
+ *         multiples of 3 & 5 with FizzBuzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  
+  
