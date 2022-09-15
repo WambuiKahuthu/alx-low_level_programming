ccf1dff3841a92f0eb719837561e296fa1e5319f
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-15 06:22:14 -0700

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line of specified length
+ *
+ * @n: length of line
+ */
+
+void print_diagonal(int n)
+{
+	int i, l;
+
+	for (i = 0; i < n; i++)
+	{
+		for (l = 0; l < i; l++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+
+		if (i <= (n - 1))
+			_putchar('\n);
+	}
+}
