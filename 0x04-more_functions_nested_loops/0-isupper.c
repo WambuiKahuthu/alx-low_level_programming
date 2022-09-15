110829b5d7272e1855cd71b2ac707af7161341e7
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-15 04:07:35 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * _isupper - checks if a character is lowercase letter
+ *
+ * @c: character
+ *
+ * Return: 1 if uppercase, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (i >= 65 && i <= 90)
+		return (1);
+	else
+		return (0);
+
+	_putchar('\n');
+}
