58908e3ede451b406ca4d00af12cd3484356422f
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-15 04:48:41 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 but not 2 & 4
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		
+		if (i != 50 && i != 52)
+
+		_putchar(i);
+
+	_putchar('\n');
+}
