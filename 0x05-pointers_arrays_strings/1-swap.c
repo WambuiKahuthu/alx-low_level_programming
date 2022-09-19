9000b762f1f396d4ed34cdb717bee6d7d93dd897
WambuiKahuthu <wkahuthu@gmail.com>
2022-09-19 03:51:47 -0700

    Betty changes

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/*
-  swap_int - swaps values of two integers
-
-  @a: integer 1
-  @b: interger 2
+/**
+ * swap_int - swaps values of two integers
+ *
+ * @a: integer 1
+ * @b: interger 2
 */
 
 void swap_int(int *a, int *b)
 {
-	int x = *a
+	int x = *a;
 
 	*a = *b;
 	*b = x;
