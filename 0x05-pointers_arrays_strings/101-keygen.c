cc79342fe5736706d249bca689d60eeefbf85c0b
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-20 00:35:01 +0300

    Error

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	srand(time(NULL));
 
-	int index = 0;
+	int index;
 	
 	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
 	for(index = 0; index < 12; index++)
