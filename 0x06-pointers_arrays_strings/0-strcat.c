a83090a993844f2a624378ba7ff8930eff9f6bcd
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-22 21:48:18 +0300

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0;
+
+	while (dest[c])
+		c++;
+
+	for (y = 0; src[y] ; y++)
+		
+		dest[x++] = src[y];
+
+	return (dest);
+}
