5a492465170c2a8a807ab05946c60cdadb97fc43
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-26 22:39:21 +0300

    Strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,12 +14,12 @@ unsigned int _strspn(char *s, char *accept)
 	while (s[i] != '\0')
 	{
 		j = 0;
-		f = 1; /*flag status*/
+		f = 1;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
-				f = 0; /*success*/
+				f = 0;
 				break;
 			}
 			j++;
