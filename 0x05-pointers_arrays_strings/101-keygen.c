9cfaaefd74c2930f8e768905969004357deb3879
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-26 23:56:27 +0300

    Generate random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define NUM_MAX 2772
+#define NUM_MIN 64
+
+/**
+ * main - entry point
+ * main - generate random values up to 2772
+ *
+ * Return: 0
+ * Return: 0 success
+ */
 
 int main(void)
 {
-	int myrand;
-	int count;
-	int total;
+	srand((unsigned int)(time(NULL)));
+	int i = 0;
+	int seed = 0;
 
+	int index;
+
+	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
+
+	for(index = 0; index < 12; index++)
 	srand(time(NULL));
-	for (count = 0, total = 2772; total > 122; count++)
-	{
-		myrand = (rand() % 125) + 1;
-		printf("%c", myrand);
-		total -= myrand;
-	}
-	printf("%c", total);
 
+	for (; seed <= (NUM_MAX - NUM_MIN); seed += i)
+	{
+		printf("%c", characters[rand() % (sizeof characters - 1)]);
+		i = (rand() % (90 - 65) + 65);
+		putchar(i);
+	}
+
+	printf("%c", NUM_MAX - seed);
 	return (0);
 }
