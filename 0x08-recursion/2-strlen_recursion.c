bf44dadaa0533725150d95a059835ddc04d3552b
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-29 00:46:32 +0300

    Length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: given string
+ *
+ * Return: length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
