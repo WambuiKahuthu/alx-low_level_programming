00fdd36c25e9656fff2530f56018f9a018cffea7
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-09-29 01:50:14 +0300

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,32 @@
 /**
+ * square_root - finds square root
+ *
+ * @n: given number
+ * @sqrt: square root
+ *
+ * Return: square root or -1
+ */
+int square_root(int n, int sqrt)
+{
+  if (sqrt * sqrt  == n)
+	  return (sqrt);
+
+  else if (sqrt * sqrt < n)
+
+	  return  (square_root(n, sqrt + 1));
+
+  else
+	  return (-1);
+}
+
+/**
  * _sqrt_recursion - find natural square root
  *
  * @n: given number
  *
  * Return: square root or -1
  */
-
 int _sqrt_recursion(int n)
 {
-return (square(n, 1));
-}
-
-/**
- * square - find square root
- *
- * @n: int to find square root
- * @sqrt: square root
- *
- * Return: square root or -1
- */
-
-int square(int n, int sqrt)
-{
-  if (sqrt * sqrt  == n)
-	  return (sqrt);
-  else if (sqrt * sqrt < n)
-	  return  (square(n, sqrt + 1));
-  else
-	  return (-1);
+	return (square_root(n, 1));
 }
