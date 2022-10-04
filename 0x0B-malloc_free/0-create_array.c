60981960e8f5c60bdaebaf2845309b2fbde7aa1a
WambuiKahuthu <wkahuthu@gmail.com>
2022-10-04 12:48:49 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-char *m = malloc(size);
-if (size == 0 || m == 0)
-	return (0);
+	char *m = malloc(size);
+	if (size == 0 || m == 0)
+		return (0);
 
-while (size--)
-	m[size] = c;
+	while (size--)
+		m[size] = c;
 
-return (m);
+	return (m);
+}
