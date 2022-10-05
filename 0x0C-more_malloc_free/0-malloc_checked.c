dc08af7910877ee0e9548c7e22c24ee5af1f117d
WambuiKahuthu <78378533+WambuiKahuthu@users.noreply.github.com>
2022-10-06 00:38:16 +0300

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: amount of bytes
+ *
+ * Return: pointer to new allocated memory
+ *         exit with 98 if malloc fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = malloc(b);
+	if (m == NULL)
+		exit(98);
+	return (m);
+}
